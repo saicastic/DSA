11c3e45d4e03f162539ebbe91831fdde6441a85c
saicastic <saiprithvisuperior@gmail.com>
2025-01-23 22:42:19 +0530

    added datatypes.cpp

---
+++ DSAinCPP/Basics/datatypes.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+   int a = 10;
+   float f = 10.2;
+   double d = 11.33;
+   char b = 'a';
+   string c = "how are you";
+
+   cout<<a<<endl << b<<endl<<c<<endl<<d<<endl<<f;
+
+
+    return 0;
+}
--- DSAinCPP/Basics/variables.cpp
+++ DSAinCPP/Basics/variables.cpp
@@ -4,7 +4,7 @@ int main()
 {
 
     int var;  //declaration, var is variable name here 
-    var = 20; // initialization
+    var = 20; // assignation
 
     int var5 = 50; // declaration and initialization
     int var2 = 20, var3 = 30, var4 = 40; // multi assigning
