40eaf0fbe83b6808c35488c29a7130bfac70fc23
saicastic <saiprithvisuperior@gmail.com>
2025-01-23 22:36:46 +0530

    added variables.cpp

---
+++ DSAinCPP/Basics/variables.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main()
+{
+
+    int var;  //declaration, var is variable name here 
+    var = 20; // initialization
+
+    int var5 = 50; // declaration and initialization
+    int var2 = 20, var3 = 30, var4 = 40; // multi assigning
+    
+    return 0;
+}
