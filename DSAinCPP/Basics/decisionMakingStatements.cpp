e70196e4a9fd1ea5d07665c76bb89cd6c5c9bece
saicastic <saiprithvisuperior@gmail.com>
2025-01-24 22:17:08 +0530

    added note for if else

--- DSAinCPP/Basics/decisionMakingStatements.cpp
+++ DSAinCPP/Basics/decisionMakingStatements.cpp
@@ -48,7 +48,7 @@ int main(){
         
         break;
 
-     case 20:
+    case 20:
 
         break;
     
@@ -57,6 +57,12 @@ int main(){
     }
 
     
+    // NOTE : 
+    // i . if any one of the condition in if (or) else if block satisfies, the conditions below that condition will not be executed
+
+    // ii . the key difference between if else and switch is that in if else conditions the flow of execution goes from top to bottom but in switch the flow goes directly to the condition which is true and if it has break it will come out of the switch condition
+
+    // iii. fall through : if we dont put break all the conditions below the true case will execute if it the condition is true or not and also default 
     
     
     return 0;
