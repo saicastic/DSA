7c8e877a5e536bf3db844669ea9f9bfb22eb4fbb
saicastic <saiprithvisuperior@gmail.com>
2025-01-24 21:16:43 +0530

    added operations.cpp

---
+++ DSAinCPP/Basics/operators.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int main(){
+
+   // arithmetic           ->       +  -  *  %  /
+   // realtional           ->       ==  >=  <=  >  <  != 
+   // logical              ->       &&  !  ||
+   // Bitwise              ->       &  ^  ~  <<  >>
+   // Assignment           ->       =  +=  -=  *=  %=  /=  ^=  <<=  >>=
+   // increment            ->       ++
+   // decrement            ->       --
+   // conditional          ->       (condition)? true : false ;
+   // comma                ->       ,
+   // pointer              ->        *(value at address), &(address)
+   // sizeof               ->        sizeof(a)
+   // typecast             ->        static_cast, dynamic_cast, const_cast, reinterpret_cast
+   // scope resolution     ->        ::
+   // member access op     ->         .   ->
+   // new and delete       ->         new,  delete  
+  
+
+    return 0;
+}
