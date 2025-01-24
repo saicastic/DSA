547bb9eb16b0a2c94111b11a59fba3a0b849ef46
saicastic <saiprithvisuperior@gmail.com>
2025-01-24 21:55:37 +0530

    added decisionMakingStatements.cpp

---
+++ DSAinCPP/Basics/decisionMakingStatements.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+int main(){
+
+    // if statement
+    if (true)
+    {
+        /* code */
+    }
+
+
+
+    // if else statement
+    if (true)
+    {
+        /* code */
+    }
+    else
+    {
+        /* code */
+    }
+
+
+   // else if ladder
+    if (true)
+    {
+        /* code */
+    }
+    else if (true)
+    {
+        /* code */
+    }
+    else
+    {
+        /* code */
+    }
+    
+    
+    
+    return 0;
+}
