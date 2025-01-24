fdb4551f72667f476fb7c478d3b7a682c41691c9
saicastic <saiprithvisuperior@gmail.com>
2025-01-24 22:25:36 +0530

    updated loops.cpp

--- DSAinCPP/Basics/loops.cpp
+++ DSAinCPP/Basics/loops.cpp
@@ -4,11 +4,62 @@ using namespace std;
 
 int main(){
 
-    // for loop
+    //i. for loop
+
     for (int i = 0; i < 10; i++)
     {
-        cout<<i;
+        /* code */
     }
+
+    //ii. while loop
+
+    int i;
+
+    while (i<10)
+    {
+     
+        /* code */
+
+        i++; // increment
+    }
+
+
+    //iii. do while loop
+
+    int k;
+
+    do
+    {
+        
+    } while (k< 10);
+
+
+    // iv. infinite loops
+
+    while (true)
+    {
+        /* code */
+    }
+
+    for ( ; ; )
+    {
+        /* code */
+    }
+
+    //v. range based "for" loop
+
+    int arr[]={1,2,3,4}
+
+    for (int num : arr)
+    {
+        cout << num << " ";
+    }
+    
+    
+    
+    
+    
+
     
     return 0;
 }
